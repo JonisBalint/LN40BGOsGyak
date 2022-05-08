a2e1d85aac8a651b58cc404468adbc4eaaa6de61
Jbalint <91829380+JonisBalint@users.noreply.github.com>
2022-05-08 14:24:16 +0200

    Add files via upload

---
+++ OSSemTask_LN40BG/beadando.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main()
+{
+	int ret;
+	pid_t pid;
+
+	putenv("RUNNABLE_PROG=LN40BG_openclose");
+	putenv("PATH=/home/jb/LN40BG/LN40BG_openclose");
+
+	pid = fork();
+	if(pid == 0){
+		printf("Gyerek vagyok, pidem:%d, szulom pidje:%d\n", getpid(), getppid());
+		ret = execl(getenv("PATH"), getenv("RUNNABLE_PROG"), NULL);
+		printf("\n");
+	}
+	else{
+		printf("Szulo vagyok, pidem:%d, gyerek pidje:%d\n", getpid(), pid);
+	}
+
+	wait(NULL);
+
+	exit(EXIT_SUCCESS);
+}
